a21553e19c0a10b301bd43dc5dd666dff3f00aab
Michael Carroll <mjcarroll@intrinsic.ai>
2025-05-30 21:46:20 +0000

    Experiment with updating to latest release
    Signed-off-by: Michael Carroll <mjcarroll@intrinsic.ai>

--- intrinsic_sdk_cmake/cmake/fetch_sdk.cmake
+++ intrinsic_sdk_cmake/cmake/fetch_sdk.cmake
@@ -3,14 +3,15 @@
 set(SDK_VERSION_JSON_FILE "${CMAKE_CURRENT_SOURCE_DIR}/cmake/sdk_version.json")
 file(READ "${SDK_VERSION_JSON_FILE}" sdk_version_json)
 string(JSON sdk_version GET ${sdk_version_json} "sdk_version")
-message(STATUS "intrinsic-ai/sdk version: ${sdk_version}")
+string(JSON sdk_checksum GET ${sdk_version_json} "sdk_checksum")
+message(STATUS "intrinsic-ai/sdk version: ${sdk_version} ${sdk_checksum}")
 
 include(FetchContent)
 # Fetch the intrinsic sdk source code during configure stage.
 FetchContent_Declare(
   intrinsic_sdk
   URL https://github.com/intrinsic-ai/sdk/archive/refs/tags/${sdk_version}.tar.gz
-  URL_HASH SHA256=0de09dec1340474c861b6b288e4bc142300ac277031b31aa09e1cebc32ea9f5b
+  URL_HASH ${sdk_checksum}
   DOWNLOAD_EXTRACT_TIMESTAMP FALSE
   PATCH_COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/cmake/sdk_patches/apply_patch.sh
     ${CMAKE_CURRENT_SOURCE_DIR}/cmake/sdk_patches/001_zenoh_helpers_cc_no_runfiles.patch
