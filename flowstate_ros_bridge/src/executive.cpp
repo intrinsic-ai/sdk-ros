80c542681486908e31d89a90e78395d26e5e48c9
William Woodall <wjwwood@google.com>
2025-12-04 18:00:11 -0600

    update license to apache-2.0 everywhere (#74)
    * update license to apache-2.0 everywhere
    
    Signed-off-by: William Woodall <wjwwood@intrinsic.ai>
    
    * remove disclaimer section
    
    Signed-off-by: William Woodall <wjwwood@intrinsic.ai>
    
    ---------
    
    Signed-off-by: William Woodall <wjwwood@intrinsic.ai>

--- flowstate_ros_bridge/src/executive.cpp
+++ flowstate_ros_bridge/src/executive.cpp
@@ -1,27 +1,16 @@
 // Copyright 2024 Intrinsic Innovation LLC
 //
-// You are hereby granted a non-exclusive, worldwide, royalty-free license to
-// use, copy, modify, and distribute this Intrinsic SDK in source code or binary
-// form for use in connection with the services and APIs provided by Intrinsic
-// Innovation LLC (“Intrinsic”).
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// If you use this Intrinsic SDK with any Intrinsic services, your use is
-// subject to the Intrinsi Platform Terms of Service
-// [https://intrinsic.ai/platform-terms].  If you create works that call
-// Intrinsic APIs, you must agree to the terms of service for those APIs
-// separately. This license does not grant you any special rights to use the
-// services.
-//
-// This copyright notice shall be included in all copies or substantial portions
-// of the software.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include "flowstate_ros_bridge/executive.hpp"
 
