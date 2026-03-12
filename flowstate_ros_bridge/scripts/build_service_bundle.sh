69c6feafee2d9be8ac7219de682a537f0448bcff
Yadunund <yadunund@gmail.com>
2026-03-12 00:48:38 +0000

    Fix Dockerfile.service and remove redundant Dockerfile in flowstate_ros_bridge
    Signed-off-by: Yadunund <yadunund@gmail.com>

--- flowstate_ros_bridge/scripts/build_service_bundle.sh
+++ flowstate_ros_bridge/scripts/build_service_bundle.sh
@@ -6,5 +6,8 @@ fi
 
 set -o errexit
 set -o verbose
-src/sdk-ros/scripts/build_container.sh --service_name flowstate_ros_bridge --service_package flowstate_ros_bridge --dockerfile src/sdk-ros/flowstate_ros_bridge/Dockerfile.service
+src/sdk-ros/scripts/build_container.sh \
+  --service_name flowstate_ros_bridge \
+  --service_package flowstate_ros_bridge \
+  --dependencies nlohmann-json3-dev
 src/sdk-ros/scripts/build_bundle.sh --service_name flowstate_ros_bridge --service_package flowstate_ros_bridge
