c4607fc35826cacaf3746a75ba6248da063836be
Brandon Corry <brandoncorry@intrinsic.ai>
2026-04-01 21:13:27 +0000

    Migrate bundle generation to inbuild tool and remove manifest/config generation from Dockerfile and CMakeLists.

--- flowstate_ros_bridge/scripts/build_service_bundle.sh
+++ flowstate_ros_bridge/scripts/build_service_bundle.sh
@@ -10,4 +10,8 @@ src/sdk-ros/scripts/build_container.sh \
   --service_name flowstate_ros_bridge \
   --service_package flowstate_ros_bridge \
   --dependencies nlohmann-json3-dev
-src/sdk-ros/scripts/build_bundle.sh --service_name flowstate_ros_bridge --service_package flowstate_ros_bridge
+src/sdk-ros/scripts/build_bundle.sh \
+  --service_name flowstate_ros_bridge \
+  --service_package flowstate_ros_bridge \
+  --manifest_path src/sdk-ros/flowstate_ros_bridge/flowstate_ros_bridge.manifest.textproto \
+  --default_config src/sdk-ros/flowstate_ros_bridge/flowstate_ros_bridge_default_config.pbtxt
