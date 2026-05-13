bfeb011dbc884aefd40e47cc80dd7904c73db87e
Brandon Corry <brandoncorry@intrinsic.ai>
2026-05-13 21:41:34 +0000

    Upgrade Gazebo dependencies, simplify gRPC lookup, and apply glibc compatibility patch.

--- intrinsic_sdk_cmake/cmake/sdk_patches/apply_all_patches.sh
+++ intrinsic_sdk_cmake/cmake/sdk_patches/apply_all_patches.sh
@@ -3,6 +3,7 @@ SCRIPT_DIR=$(dirname "$0")
 ${SCRIPT_DIR}/apply_patch.sh \
   ${SCRIPT_DIR}/001_zenoh_helpers_cc_no_runfiles.patch \
   ${SCRIPT_DIR}/002_gcc13_publisher_noexcept.patch \
-  ${SCRIPT_DIR}/003_fix_return_types.patch
+  ${SCRIPT_DIR}/003_fix_return_types.patch \
+  ${SCRIPT_DIR}/003_icon_strerror_glibc2_39.patch
 
 python3 ${SCRIPT_DIR}/fix_cwiseLess.py
