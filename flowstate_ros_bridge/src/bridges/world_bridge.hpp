09869de31ac319cd0729db5bd6d0400d68cbea09
William Woodall <wjwwood@google.com>
2026-09-16 15:15:09 -0500

    Conditionally use resource_retriever_interfaces in Lyrical and preserve rviz_http_proxy in Jazzy (#152)
    * Add Lyrical-compatible rviz resource retriever service
    
    Signed-off-by: Morgan Quigley <morganquigley@intrinsic.ai>
    Signed-off-by: William Woodall <wjwwood@google.com>
    
    * Use the Lyrical rviz resource retriever client to get rid of rviz_http_proxy
    
    Signed-off-by: Morgan Quigley <morganquigley@intrinsic.ai>
    Signed-off-by: William Woodall <wjwwood@google.com>
    
    * Add resource_retriever_interfaces linkage
    
    Signed-off-by: Morgan Quigley <morganquigley@intrinsic.ai>
    Signed-off-by: William Woodall <wjwwood@google.com>
    
    * Conditionally use resource_retriever_interfaces in Lyrical and preserve rviz_http_proxy in Jazzy
    
    - Use condition="$ROS_DISTRO != jazzy" for resource_retriever_interfaces in package.xml.
    - In CMakeLists.txt, only require resource_retriever_interfaces and define FLOWSTATE_ROS_BRIDGE_USE_RESOURCE_RETRIEVER_INTERFACES when ROS_DISTRO != jazzy.
    - Conditionally select resource_retriever_interfaces vs flowstate_interfaces in world_bridge.hpp.
    - Default mesh URL prefix to service:///flowstate_get_resource:/ on Lyrical and http://localhost:8123/ on Jazzy.
    - Restore scripts/rviz_http_proxy and its CMake install target for Jazzy.
    
    Signed-off-by: William Woodall <wjwwood@google.com>
    
    ---------
    
    Signed-off-by: Morgan Quigley <morganquigley@intrinsic.ai>
    Signed-off-by: William Woodall <wjwwood@google.com>
    Co-authored-by: Morgan Quigley <morganquigley@intrinsic.ai>

--- flowstate_ros_bridge/src/bridges/world_bridge.hpp
+++ flowstate_ros_bridge/src/bridges/world_bridge.hpp
@@ -22,10 +22,14 @@
 #include "absl/container/flat_hash_set.h"
 #include "absl/strings/str_split.h"
 #include "absl/synchronization/mutex.h"
-#include "flowstate_interfaces/srv/get_resource.hpp"
 #include "flowstate_ros_bridge/bridge_interface.hpp"
 #include "geometry_msgs/msg/wrench_stamped.hpp"
 #include "rclcpp/rclcpp.hpp"
+#if defined(FLOWSTATE_ROS_BRIDGE_USE_RESOURCE_RETRIEVER_INTERFACES)
+#include "resource_retriever_interfaces/srv/get_resource.hpp"
+#else
+#include "flowstate_interfaces/srv/get_resource.hpp"
+#endif
 #include "sensor_msgs/msg/joint_state.hpp"
 #include "tf2_msgs/msg/tf_message.hpp"
 #include "visualization_msgs/msg/marker_array.hpp"
@@ -39,7 +43,11 @@ class WorldBridge : public BridgeInterface {
  public:
   ~WorldBridge();
 
+#if defined(FLOWSTATE_ROS_BRIDGE_USE_RESOURCE_RETRIEVER_INTERFACES)
+  using GetResource = resource_retriever_interfaces::srv::GetResource;
+#else
   using GetResource = flowstate_interfaces::srv::GetResource;
+#endif
 
   /// Documentation inherited.
   void declare_ros_parameters(ROSNodeInterfaces ros_node_interfaces) final;
